941d5cb09600636db9d7c4a92be3232347d0fd62
sub77 <sub77@ymail.com>
2017-10-26 00:30:02 +0000

    ffritz: atom: buildroot: nano

--- packages/x86/ffritz/etc/ffritz-env.sh
+++ packages/x86/ffritz/etc/ffritz-env.sh
@@ -4,5 +4,7 @@
 EXEC_DIR=$( readlink -f $( dirname -- "$0" ) )
 export FFINSTDIR=`cd $EXEC_DIR/..; pwd`
 export FFRITZ_HOME=/var/media/ftp/ffritz
+export TERMINFO=/usr/share/terminfo
+export TERM=vt220
 PATH=$PATH:$FFINSTDIR/bin
 FFLOG=/var/log/ffritz.log
